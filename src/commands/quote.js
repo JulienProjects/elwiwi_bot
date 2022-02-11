9405d3944d6f3541420fd7066fed0f109a77b03f
Julien <julien.mueller99@web.de>
2022-02-11 22:01:24 +0100

    quote command

---
+++ src/commands/quote.js
@@ -0,0 +1,13 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { getQuote } from "generate-quote";
+
+export default {
+	data: new SlashCommandBuilder()
+		.setName('quote')
+		.setDescription('Send some el wiwi quote'),
+	async execute(interaction) {
+        const quote = getQuote();
+        console.log(quote);
+		await interaction.reply(quote.text + " - el wiwi");
+	},
+};
