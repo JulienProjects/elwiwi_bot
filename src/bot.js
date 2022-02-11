8a0ebe3cc351249f88a1db32384cace809357da9
Julien <julien.mueller99@web.de>
2022-02-11 19:18:05 +0100

    12 hours praises

--- src/bot.js
+++ src/bot.js
@@ -4,6 +4,7 @@ dotenv.config()
 import  {getQuote} from "generate-quote";
 import { Client, Intents } from 'discord.js';
 
+import { CronJob } from 'cron';
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
@@ -13,11 +14,11 @@ client.on("ready", () => {
 })
 
 client.on("message", (msg) => {
-    if(msg.content === ":elwiwiright:"){
-        msg.reply("Yes Praise me :elwiwiright:");
+    if(msg.content === "<:elwiwiright:915250236555923578>"){
+        msg.reply("Yes Praise me <:elwiwiright:915250236555923578>");
     }
 })
-
+//daily-appreciation-el-wiwi
 function dailyMessage(){
     const channel = client.channels.cache.find(channel => channel.name === "daily-appreciation-el-wiwi")
 
@@ -28,9 +29,12 @@ function dailyMessage(){
     }
 }
 
+var job = new CronJob('* * */12 * * *', function() {
+   dailyMessage();
+  }, null, true, 'America/Los_Angeles');
+
+
 
 client.login(process.env.BOT_TOKEN_ELWIWI);
 
-// setInterval(() => {
-//     dailyMessage();
-// }, 100000)
+job.start();
