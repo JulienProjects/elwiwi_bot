1e918f930401b9300153f005702d3dd823cfcccd
Julien <julien.mueller99@web.de>
2022-03-01 08:12:40 +0100

    inform if praised

--- src/commands/streak.js
+++ src/commands/streak.js
@@ -11,7 +11,12 @@ export default {
             if(!data || Object.keys(data).length === 0){
                 interaction.author.send("No Praises yet, start praising el wiwi in the daily-appreciation-el-wiwi channel. Man real talk wtf?..")
             }else{
-                interaction.author.send(`Your on streak ${data.streak} goob job`);
+                const last_praiseDate = new Date(data.last_praise);
+                var today = new Date();   
+                
+                const isToday = last_praiseDate.getDate() === today.getDate() && last_praiseDate.getMonth() === today.getMonth() && last_praiseDate.getFullYear() === today.getFullYear();
+                const praisesToday = isToday ? "You did your Praise Today" : "You still need to Praise for Today";
+                interaction.author.send(`Your on streak ${data.streak} goob job. ${praisesToday}`);
             }
         })
 	},
