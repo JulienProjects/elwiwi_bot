b1c46c05a4bcbb91a68c587a22a964e208833474
Julien <julien.mueller99@web.de>
2022-03-02 13:45:00 +0100

    fixmyproblems command

---
+++ src/commands/fixmyproblems.js
@@ -0,0 +1,10 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+
+export default {
+	data: new SlashCommandBuilder()
+		.setName('fixmyproblems')
+		.setDescription('fixes all your problems'),
+	async execute(interaction) {
+		await interaction.reply("KEKW i cant help here");
+	},
+};
