ae9919f495cbde37714576d57983a70d2d410191
Julien <julien.mueller99@web.de>
2022-03-18 15:26:13 +0100

    eslint

--- src/commands/fixmyproblems.js
+++ src/commands/fixmyproblems.js
@@ -1,10 +1,10 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
 
 export default {
-	data: new SlashCommandBuilder()
-		.setName('fixmyproblems')
-		.setDescription('fixes all your problems'),
-	async execute(interaction) {
-		await interaction.reply("KEKW i cant help here");
-	},
+    data: new SlashCommandBuilder()
+        .setName('fixmyproblems')
+        .setDescription('fixes all your problems'),
+    async execute(interaction) {
+        await interaction.reply("KEKW i cant help here");
+    },
 };
